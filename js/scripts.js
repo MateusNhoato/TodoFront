8099ecc883d5e2ac3dce19c6b5b233ee9e1cb186
Mateus Nhoato <manhoato@gmail.com>
2023-03-04 12:01:26 -0300

    Implementing CRUD

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
+const novoTodoContainer = document.querySelector('#novo-todo-container');
 const adicionarTodoBotao = document.querySelector("#add-button");
 const novoTodoInput = document.querySelector("#novo-todo");
 const descricaoInput = document.querySelector("#descricao");
+const editarTodoBotao = document.querySelector('#edit-button');
 const todosContainer = document.querySelector("#todos-container");
 
 function adicionarTodoNoDocumento(todo){
-    let bodyData = {
-        "id": todo['id'],
-        "nome": todo['nome'],
-    "descricao": todo['descricao'],
-    "concluida": todo['concluida']
-    }
-
     const nomeTodo = document.createElement('h3');
     nomeTodo.innerText = todo['nome'];
 
@@ -23,7 +18,7 @@ function adicionarTodoNoDocumento(todo){
     novoTodo.classList.add("todo-container");
     
     const botaoRemover = document.createElement('button');
-    botaoRemover.classList.add("remove");
+    botaoRemover.classList.add("remover");
     botaoRemover.innerHTML= '<img src="icons/cross.png" alt="remover" >';
 
     
@@ -32,25 +27,31 @@ function adicionarTodoNoDocumento(todo){
     });
 
     const botaoCheck = document.createElement('button');
-    botaoCheck.classList.add("add");
+    botaoCheck.classList.add("check");
     botaoCheck.innerHTML= '<img src="icons/check.png" alt="check" >';
 
     botaoCheck.addEventListener('click', async function(event){
-        mudarConcluidaTodo(event, todo['id'], bodyData, novoTodo)
+        mudarConcluidaTodo(event, todo['id'], todo, novoTodo)
     });
 
     const botaoEditar = document.createElement('button');
     botaoEditar.innerHTML= '<img src="icons/edit.png" alt="editar" >';
-    botaoEditar.classList.add("edit");
+    botaoEditar.classList.add("editar");
     
     botaoEditar.addEventListener('click', async function(event){
-        editarTodo(event, todo['id'], bodyData)
+        editarTodo(event, todo['id'], todo, novoTodo)
     });
 
 
     const botaoReativar = document.createElement('button');
     botaoReativar.innerHTML= '<img src="icons/return.png" alt="reativar" >';
     botaoReativar.classList.add("reativar");
+
+    botaoReativar.addEventListener('click', async function(e){
+        mudarConcluidaTodo(e, todo['id'], todo, novoTodo);
+    });
+
+
 
     novoTodo.appendChild(nomeTodo);
     novoTodo.appendChild(descricaoTodo);
@@ -70,19 +71,21 @@ function adicionarTodoNoDocumento(todo){
     todosContainer.appendChild(novoTodo);
 };
 
+function removerTodoDoDocumento(todo){
+    todosContainer.removeChild(todo);
+}
+
 async function removeTodo(event, id, todo){
         event.preventDefault();
         try{
             const response = await fetch(`https://localhost:7104/Todos/${id}`, {
                 method: "DELETE",
             });
-            console.log(response);
-            todosContainer.removeChild(novoTodo);
         }catch { (erro) =>
             // se ocorrer erros
             console.log(erro);
         }
-        todosContainer.removeChild(todo);
+        removerTodoDoDocumento(todo);
 }
 
 async function postTodo(event){
@@ -97,7 +100,7 @@ async function postTodo(event){
             return;
         }
 
-    let bodyData = {
+    let novoTodo = {
         "nome": `${todo}`,
     "descricao": `${desc}`,
     "concluida": false
@@ -111,10 +114,10 @@ async function postTodo(event){
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(bodyData),
+                body: JSON.stringify(novoTodo),
             });
             const data = await response.json();
-            bodyData['id'] = data['id'];
+            novoTodo['id'] = data['id'];
 
             console.log(response);
         }catch { (erro) =>
@@ -126,7 +129,7 @@ async function postTodo(event){
    
     novoTodoInput.value = "";
     descricaoInput.value = "";
-    adicionarTodoNoDocumento(bodyData);
+    adicionarTodoNoDocumento(novoTodo);
 }
 
 async function mudarConcluidaTodo(event, id, todoBody, todoDocument){
@@ -139,9 +142,10 @@ async function mudarConcluidaTodo(event, id, todoBody, todoDocument){
                 todoBody['concluida'] = false;
 
                 // documento
+                todoDocument.classList.remove('concluida');
                 todoDocument.querySelector('.reativar').hidden = true;
-                todoDocument.querySelector('.add').hidden = false;
-                todoDocument.querySelector('.edit').hidden = false;           
+                todoDocument.querySelector('.check').hidden = false;
+                todoDocument.querySelector('.editar').hidden = false;           
             }
         else
             {
@@ -149,9 +153,10 @@ async function mudarConcluidaTodo(event, id, todoBody, todoDocument){
                 todoBody['concluida'] = true;
 
                 // documento
+                todoDocument.classList.add('concluida');
                 todoDocument.querySelector('.reativar').hidden = false;
-                todoDocument.querySelector('.add').hidden = true;
-                todoDocument.querySelector('.edit').hidden = true;
+                todoDocument.querySelector('.check').hidden = true;
+                todoDocument.querySelector('.editar').hidden = true;
             }
 
         try{
@@ -170,16 +175,55 @@ async function mudarConcluidaTodo(event, id, todoBody, todoDocument){
     }
 }
 
-async function editarTodo(event, id, todo)
+async function editarTodo(event, id, todo, todoDocument)
     {
         event.preventDefault();
-        
+
         descricaoInput.value = todo['descricao'];
         novoTodoInput.value = todo['nome'];
         
-        const confirmarEdicao = document.createElement('button');
-        confirmarEdicao.addEventListener('click', async function(e){
+        editarTodoBotao.hidden = false;
+        adicionarTodoBotao.hidden = true;
 
+        editarTodoBotao.addEventListener('click', async (e) =>{
+            e.preventDefault();
+            
+            // editando as informações
+            const nomeEditado = novoTodoInput.value;
+            const descricaoEditada = descricaoInput.value;
+            
+            if(nomeEditado.trim() === "" || descricaoEditada.trim() === "")
+            {
+                alert("Informações inválidas.");
+                return;
+            }
+    
+        let todoBody = {
+            "id": id,
+            "nome": `${nomeEditado}`,
+        "descricao": `${descricaoEditada}`,
+        "concluida": false
+        }
+            try{
+                const response = await fetch(`https://localhost:7104/Todos/${id}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(todoBody),
+                });
+            }catch { (erro) =>
+                // se ocorrer erros
+                console.log(erro);
+            }
+
+            editarTodoBotao.hidden = true;
+            adicionarTodoBotao.hidden = false;
+            novoTodoInput.value = "";
+            descricaoInput.value = "";
+
+            removerTodoDoDocumento(todoDocument);
+            adicionarTodoNoDocumento(todoBody);
     });
 }
 
